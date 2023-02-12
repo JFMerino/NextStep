c663e2104b6b57a2d7da342e6f4e16b5d943824a
JFMerino <jrfm.meri@gmail.com>
2023-02-12 11:36:41 -0500

    terminado el footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "./styles/footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {} from '@fortawesome/free-regular-svg-icons';
-import {} from '@fortawesome/free-solid-svg-icons'
 
 export const Footer = () => {
   return (
@@ -10,19 +7,18 @@ export const Footer = () => {
       <div className="footer">
         <div className="container-footer">
           <div className="line-footer">
-          <div className="encabezado">
-            <h1 className="titulo-footer">
-              "La perseverancia es la madre del éxito."
-            </h1>
-            <input
-              type="submit"
-              value="Mas informacion"
-              className="more-info"
-            />
+            <div className="encabezado">
+              <h1 className="titulo-footer">
+                "La perseverancia es la madre del éxito."
+              </h1>
+              <input
+                type="submit"
+                value="Mas informacion"
+                className="more-info"
+              />
+            </div>
           </div>
-          </div>          
           <div className="redes">
-          <FontAwesomeIcon icon={["fab", "github"]} />
           </div>
           <div className="copy">
             <p>&copy; 2023 NextStep, Todos los derechos reservados</p>
